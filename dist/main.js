60acc25c861b8b15231bf5ecb6fc2438900d037e
Inmersys <developer@inmersys.com>
2021-06-09 00:37:38 -0500

    .

--- dist/main.js
+++ dist/main.js
@@ -2,10 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const fs = require("fs");
 const body_parser_1 = require("body-parser");
 const helmet = require("helmet");
+const crPath = '/etc/letsencrypt/live/testapi.leiva.io/fullchain.pem';
+const pkPath = '/etc/letsencrypt/live/testapi.leiva.io/privkey.pem';
+const options = {};
+if (fs.existsSync(crPath) && fs.existsSync(pkPath)) {
+    options.httpsOptions = {
+        cert: fs.readFileSync(crPath),
+        key: fs.readFileSync(pkPath)
+    };
+}
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    console.log({ options });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, options);
     app.use(body_parser_1.json({ limit: '10mb' }));
     app.use(helmet());
     app.enableCors();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 import { json } from 'body-parser';
 import * as helmet from 'helmet';
+const crPath = '/etc/letsencrypt/live/testapi.leiva.io/fullchain.pem';
+const pkPath = '/etc/letsencrypt/live/testapi.leiva.io/privkey.pem';
+const options: any = {};
+
+if (fs.existsSync(crPath) && fs.existsSync(pkPath)) {
+  // cargamos los archivos sobre las options
+  options.httpsOptions = {
+    cert: fs.readFileSync(crPath),
+    key: fs.readFileSync(pkPath)
+  }
+
+}
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
+  console.log({ options })
+  const app = await NestFactory.create(AppModule, options);
   app.use(json({ limit: '10mb' }));
   app.use(helmet());
   app.enableCors();
